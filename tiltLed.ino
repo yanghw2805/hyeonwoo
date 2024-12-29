5e665cf496eea73fda935f1ce22913240f637ecf
yanghw2805 <dannyyang2805@gmail.com>
2024-12-30 00:59:53 +0900

    Add files via upload

---
+++ tiltLed.ino
@@ -0,0 +1,26 @@
+
+void setup() {
+  pinMode(7, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+
+void loop() {
+  
+  int tilt = digitalRead(7);  
+  Serial.println(tilt);
+  
+  if (tilt == 0) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
+
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+  }
+  delay(10);
+}
+ 
